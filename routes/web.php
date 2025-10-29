1e26229f7c3ac4681c446960126bc142183a903e
Kyuusha <daaddimaki04@gmail.com>
2025-10-29 23:03:38 +0700

    update route & title app

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminObatController;
 use App\Http\Controllers\JadwalPeriksaController;
 use App\Http\Controllers\JadwalController;
-// Halaman utama
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 
 Route::prefix('admin')->middleware('auth')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
@@ -50,8 +44,8 @@ Route::prefix('admin')->middleware('auth')->group(function () {
 
 
 // Routes untuk Authentication: Login, Register, Logout
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
